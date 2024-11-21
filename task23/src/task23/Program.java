f340e1799c8d1c0b1b75b7de2cb9cd6243da2b9f
b1020015b <b1020015b@gmail.com>
2024-11-21 22:54:29 +0300

    Add files via upload

---
+++ task23/src/task23/Program.java
@@ -0,0 +1,13 @@
+package task23;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		for (int evenNumber : nums)
+	        if (evenNumber % 2 == 0)
+	            System.out.println(evenNumber);
+		
+	}
+
+}
